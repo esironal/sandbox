1b84d6caddd2aca64d27a568679146e3bfe33ce9
Fantasy Shao <forever.fantasy27@gmail.com>
2015-02-02 19:46:54 +0800

    Update modules.

--- ES6/dest/modules.js
+++ ES6/dest/modules.js
@@ -1,15 +1,5 @@
 "use strict";
 
-/**
- *
- * modules.js
- *
- * @description
- * @author Fantasy <fantasyshao@icloud.com>
- * @create 2015-01-31
- * @update 2015-01-31
- */
-
 var square = require("./arrow").square;
 
 
--- ES6/src/modules.js
+++ ES6/src/modules.js
@@ -1,13 +1,3 @@
-/**
- *
- * modules.js
- *
- * @description
- * @author Fantasy <fantasyshao@icloud.com>
- * @create 2015-01-31
- * @update 2015-01-31
- */
-
 import { square } from './arrow';
 
 console.log(square(2));
