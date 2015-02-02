63cb44719f2dfb5d2400945b9adbcb942fcf21ac
Fantasy Shao <forever.fantasy27@gmail.com>
2015-02-02 19:46:45 +0800

    ES6 - variables.

---
+++ ES6/dest/variables.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// Let expression
+for (var _i = 0; _i < 10; _i++) {
+    console.log(_i);
+}
+
+console.log(typeof i);
+
+var PI = 3.1415926;
+
+console.log(PI);
+
+// PI is read-only
+// PI = 3.14;
---
+++ ES6/src/variables.js
@@ -0,0 +1,13 @@
+// Let expression
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+console.log(typeof i);
+
+const PI = 3.1415926;
+
+console.log(PI);
+
+// PI is read-only
+// PI = 3.14;
