42d058e003f2b5bf76c3b3fef4b3da043fb3e6e7
Fantasy Shao <forever.fantasy27@gmail.com>
2015-02-09 18:11:47 +0800

    ES6 - Template string

---
+++ ES6/dest/template.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var word = "World";
+
+// Single line template string
+console.log("Hello, " + word);
+
+var weather = "Sunny";
+var temprature = "20 degree";
+
+// Multiple lines template string
+console.log("\nToday is " + weather + "\nthe temprature is " + temprature + "\n");
---
+++ ES6/src/template.js
@@ -0,0 +1,13 @@
+var word = 'World';
+
+// Single line template string
+console.log(`Hello, ${word}`);
+
+var weather = 'Sunny';
+var temperature = '20 degree';
+
+// Multiple lines template string
+console.log(`
+Today is ${weather}
+the temprature is ${temperature}
+`);
