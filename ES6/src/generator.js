4fbdade45565d736f7d21afee7767fb8d6d0b889
Fantasy Shao <forever.fantasy27@gmail.com>
2015-02-10 11:15:03 +0800

    ES6 - generator

---
+++ ES6/src/generator.js
@@ -0,0 +1,13 @@
+// Generator
+function* test () {
+  yield 'Hello';
+  yield 'World';
+  return 'Hello World';
+}
+
+var t = test();
+
+console.log(t);
+console.log(t.next());
+console.log(t.next());
+console.log(t.next());
